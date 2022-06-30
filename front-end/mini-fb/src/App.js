4703772ca7945017c5fccc84ae0495de9fdca78d
Shafiq Saleheen <bsse1125@iit.du.ac.bd>
2022-07-01 02:25:54 +0600

    Navigation bar

--- front-end/mini-fb/src/App.js
+++ front-end/mini-fb/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+import * as React from "react";
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 import Home from './Components/Home';
 
 function App() {
   return (
     <div className="App">
-        <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
